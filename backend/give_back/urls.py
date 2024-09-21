bf8b1bc7b2aac052b782940d10193ad4a56a58a8
daunelaugust <daunelaugustin@gmail.com>
2024-09-21 17:30:24 -0400

    fixed volunteer update

--- backend/give_back/urls.py
+++ backend/give_back/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import UserList, VolunteerList, OrganizationList, ClientList
+from .views import UserList, VolunteerList, OrganizationList, ClientList, update_volunteer_profile
 
 urlpatterns = [
     path('users/', UserList.as_view(), name='user-list'),
     path('volunteers/', VolunteerList.as_view(), name='volunteer-list'),
     path('organizations/', OrganizationList.as_view(), name='organization-list'),
     path('clients/', ClientList.as_view(), name='client-list'),
+    path('update-profile/', update_volunteer_profile, name='update_profile')
 ]
