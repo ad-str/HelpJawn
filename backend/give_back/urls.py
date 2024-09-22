bd8edbb5b7213638686d779670274cf57bfb62cc
daunelaugust <daunelaugustin@gmail.com>
2024-09-22 01:20:26 -0400

    fixed spelling issue

--- backend/give_back/urls.py
+++ backend/give_back/urls.py
@@ -7,12 +7,11 @@ urlpatterns = [
     path('volunteers/', VolunteerList.as_view(), name='volunteer-list'),
     path('organizations/', OrganizationList.as_view(), name='organization-list'),
     path('clients/', ClientList.as_view(), name='client-list'),
-    path('api/update_profile/<int:user_id>/', update_profile, name='update_profile'),
+    path('/update_profile/<int:user_id>/', update_profile, name='update_profile'),
     path('events/', EventList.as_view(), name='event-list'),
     path('impact-notes/', ImpactNoteList.as_view(), name='impact-note-list'),
     path('login/', login_user, name='login_user'), 
     path('logout/', logout_user, name='logout_user'),
-    path('update-profile/', update_volunteer_profile, name='update_profile'),
     path('event-signup/', event_signup, name='event-signup'),
     path('org-events/<int:user_id>/', org_events, name='org-events'),
 ]
